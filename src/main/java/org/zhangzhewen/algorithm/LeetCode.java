8a0b882cad1906aa1e46d48ccbcee40286313a2d
zhangzhewen <zhangzhewen@videopls.com>
2019-02-28 18:52:40 +0800

    完成求滑动窗口最大数算法

--- src/main/java/org/zhangzhewen/algorithm/LeetCode.java
+++ src/main/java/org/zhangzhewen/algorithm/LeetCode.java
@@ -1,7 +1,8 @@
 package org.zhangzhewen.algorithm;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.*;
 
 /**
  * 描述
@@ -24,7 +25,43 @@ public class LeetCode {
             return null;
         }
 
-    public static void main(String[] args) {
-        twoSum(new int[]{3,2,4},6);
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums==null){
+            return null;
+        }
+        Deque<Integer> window = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0 ; i < nums.length ; i ++){
+            //移除窗口外元素
+            if(i>=k&&window.getFirst()<=i-k){
+                window.removeFirst();
+            }
+            while(window.size()>0 && nums[window.getLast()]<=nums[i]){
+                window.removeLast();
+            }
+            window.add(i);
+            if(i >= k - 1){
+                result.add(nums[window.getFirst()]);
+            }
+        }
+        int [] res = new int[result.size()];
+        for(int i = 0 ;i < result.size();i++){
+            res[i] = result.get(i);
+        }
+        return res;
     }
+
+    public static void main(String[] args) {
+//        twoSum(new int[]{3,2,4},6);
+
+        /*
+        int[] result = maxSlidingWindow(new int[]{1,10,2,-1,3},2);
+        //期望的是 10 10 2 3
+        for(int i = 0;i<result.length;i++){
+            System.out.print(result[i]+" ");
+        }
+        */
+
+    }
+
 }
