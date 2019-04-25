417b958401261292287898ce43bfecbdb093e5f1
zhangzhewen <zhangzhewen@videopls.com>
2019-04-25 13:49:44 +0800

    增加计数排序

--- src/main/java/org/zhangzhewen/algorithm/sort/CountingSort.java
+++ src/main/java/org/zhangzhewen/algorithm/sort/CountingSort.java
@@ -2,56 +2,71 @@ package org.zhangzhewen.algorithm.sort;
 
 public class CountingSort extends Sortable {
 
-    // 计数排序，a是数组，n是数组大小。假设数组中存储的都是非负整数。
-    public static void countingSort(int[] a, int n) {
-        if (n <= 1) return;
+    public static int[] countingSort(int[] arr) {
 
-        // 查找数组中数据的范围
-        int max = a[0];
-        for (int i = 1; i < n; ++i) {
-            if (max < a[i]) {
-                max = a[i];
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        //找出数组中的最大最小值
+        for(int i = 0; i < arr.length; i++){
+            max = Math.max(max, arr[i]);
+            min = Math.min(min, arr[i]);
+        }
+
+        int[] help = new int[max - min + 1];
+
+        //找出每个数字出现的次数
+        for(int i = 0; i < arr.length; i++){
+            int mapPos = arr[i] - min;
+            help[mapPos]++;
+        }
+
+        //计算每个数字应该在排序后数组中应该处于的位置
+        for(int i = 1; i < help.length; i++){
+            help[i] = help[i-1] + help[i];
+        }
+
+        //根据help数组进行排序
+        int res[] = new int[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            int post = --help[arr[i] - min];
+            res[post] = arr[i];
+        }
+
+        return res;
+    }
+
+    public static int[] countingSort2(int[] arr){
+        int bias,min = arr[0],max=arr[0];
+        for(int i = 1 ; i < arr.length;i++){
+            if(min>arr[i]){
+                min = arr[i];
+            }
+            if(max<arr[i]){
+                max = arr[i];
+            }
+        }
+        bias = 0 - min;
+        int[] bucket = new int[max - min + 1];
+        for(int i = 0 ; i < arr.length ; i ++){
+            bucket[arr[i]+bias]++;
+        }
+        int index = 0,i = 0;
+        while(index < arr.length){
+            if(bucket[i]>0){
+                arr[index++]=i-bias;
+                bucket[i]--;
+            }else {
+                i++;
             }
         }
 
-        // 申请一个计数数组c，下标大小[0,max]
-        int[] c = new int[max + 1];
-        for (int i = 0; i < max + 1; ++i) {
-            c[i] = 0;
-        }
-
-        // 计算每个元素的个数，放入c中
-        for (int i = 0; i < n; ++i) {
-            c[a[i]]++;
-        }
-
-        // 依次累加
-        for (int i = 1; i < max + 1; ++i) {
-            c[i] = c[i-1] + c[i];
-        }
-
-        // 临时数组r，存储排序之后的结果
-        int[] r = new int[n];
-        // 计算排序的关键步骤了，有点难理解
-        for (int i = n - 1; i >= 0; --i) {
-            int index = c[a[i]]-1;
-            r[index] = a[i];
-            c[a[i]]--;
-        }
-
-        // 将结果拷贝会a数组
-        for (int i = 0; i < n; ++i) {
-            a[i] = r[i];
-        }
+        return arr;
     }
 
     @Override
     public int[] sort(int[] a) {
-
-        countingSort(a,a.length);
-
-
-        return a;
+        return countingSort2(a);
     }
 
 
