4edcef3951b484fd376f4a86597c86aa3f425211
zhangzhewen <zhangzhewen@videopls.com>
2019-05-31 15:27:19 +0800

    Trie.java

--- src/main/java/org/zhangzhewen/algorithm/Trie.java
+++ src/main/java/org/zhangzhewen/algorithm/Trie.java
@@ -9,55 +9,65 @@ package org.zhangzhewen.algorithm;
 public class Trie {
 
     private TrieNode root;
-    /** Initialize your data structure here. */
+
+    /**
+     * Initialize your data structure here.
+     */
     public Trie() {
         root = new TrieNode(' ');
     }
 
-    /** Inserts a word into the trie. */
+    /**
+     * Inserts a word into the trie.
+     */
     public void insert(String word) {
         TrieNode ws = root;
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            if(ws.children[c - 'a']==null){
-                ws.children[c-'a']=new TrieNode(c);
+            if (ws.children[c - 'a'] == null) {
+                ws.children[c - 'a'] = new TrieNode(c);
             }
-            ws = ws.children[c-'a'];
+            ws = ws.children[c - 'a'];
         }
-        ws.isWord=true;
+        ws.isWord = true;
     }
 
-    /** Returns if the word is in the trie. */
+    /**
+     * Returns if the word is in the trie.
+     */
     public boolean search(String word) {
         TrieNode ws = root;
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            if(ws.children[c-'a']==null){
+            if (ws.children[c - 'a'] == null) {
                 return false;
             }
-            ws = ws.children[c-'a'];
+            ws = ws.children[c - 'a'];
         }
         return ws.isWord;
     }
 
-    /** Returns if there is any word in the trie that starts with the given prefix. */
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
     public boolean startsWith(String prefix) {
         TrieNode ws = root;
         for (int i = 0; i < prefix.length(); i++) {
             char c = prefix.charAt(i);
-            if(ws.children[c-'a']==null){
+            if (ws.children[c - 'a'] == null) {
                 return false;
             }
-            ws = ws.children[c-'a'];
+            ws = ws.children[c - 'a'];
         }
         return true;
     }
 
-    class TrieNode{
+    class TrieNode {
         public char val;
         public boolean isWord;
         public TrieNode[] children = new TrieNode[26];
-        TrieNode(char c){
+
+        TrieNode(char c) {
             this.val = c;
         }
     }
