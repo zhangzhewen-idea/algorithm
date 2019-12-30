0e367e04dc70dd96a0b6af08ab4377e812e55009
zhangzhewen <zhangzhewen@videopls.com>
2019-12-30 12:05:44 +0800

    SelectionSort.java

--- src/main/java/org/zhangzhewen/algorithm/sort/SelectionSort.java
+++ src/main/java/org/zhangzhewen/algorithm/sort/SelectionSort.java
@@ -34,19 +34,7 @@ public class SelectionSort extends Sortable{
 
     public int[] sort(int[] a){
 
-        for (int i = 0; i < a.length; i++) {
-            int minIndex=i;
-            for (int j = i+1; j < a.length; j++) {
-                if(a[minIndex]>a[j]){
-                    minIndex=j;
-                }
-            }
-            if(minIndex!=i){
-                int tmp = a[minIndex];
-                a[minIndex] = a[i];
-                a[i] = tmp;
-            }
-        }
+
 
         return a;
     }
