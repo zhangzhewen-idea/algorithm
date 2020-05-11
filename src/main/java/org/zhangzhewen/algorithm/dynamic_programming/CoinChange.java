ff7fbecb7a361f60cc668c88720c6473d5500853
zhangzhewen <zhangzhewen@videopls.com>
2020-05-12 00:03:19 +0800

    CoinChange.java

---
+++ src/main/java/org/zhangzhewen/algorithm/dynamic_programming/CoinChange.java
@@ -0,0 +1,30 @@
+package org.zhangzhewen.algorithm.dynamic_programming;
+
+/**
+ * CoinChange
+ *
+ * @author zhangzhewen
+ * @date 2020/5/11
+ */
+public class CoinChange {
+    public static void main(String[] args) {
+        System.out.println(new CoinChange().coinChange(new int[]{1, 2, 5}, 11));
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = max;
+        }
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
