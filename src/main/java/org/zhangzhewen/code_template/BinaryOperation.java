09ae1a529e2a0426999f8fbd18da36fb6469c058
zhangzhewen <zhangzhewen@videopls.com>
2020-05-13 20:06:44 +0800

    code_template

---
+++ src/main/java/org/zhangzhewen/code_template/BinaryOperation.java
@@ -0,0 +1,15 @@
+package org.zhangzhewen.code_template;
+
+/**
+ * BinaryOperation
+ *
+ * @author zhangzhewen
+ * @date 2020/5/13
+ */
+public class BinaryOperation {
+    /*
+    1. X&1 == 0 OR == 1,判断奇偶(X%2 == 1)
+    2. X = X&(X-1), 清零最低位的1
+    3. X = X&(-X), 得到最低位的1
+     */
+}
---
+++ src/main/java/org/zhangzhewen/code_template/InterviewRules.java
@@ -0,0 +1,19 @@
+package org.zhangzhewen.code_template;
+
+/**
+ * InterviewRules
+ *
+ * @author zhangzhewen
+ * @date 2020/5/13
+ */
+public class InterviewRules {
+    /*
+    1. Clarification(询问所有细节、边界条件、可能的极端错误情况)
+    2. Possible Solution(所有可能的解法都和面试官沟通一遍)
+        Compare Time&Space Complexity(时间复杂度&空间复杂度)
+        Optimal Solution(最优解)
+    3. Coding(写代码，可以利用代码模版)
+    4. Test Cases(测试用例，正常数据、极端数据、特殊想不到的数据)
+    5. 沟通和交流很重要(把面试官当成未来的同事，进入Teamworking的状态)
+     */
+}
