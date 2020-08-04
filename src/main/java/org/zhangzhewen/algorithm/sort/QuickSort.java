1cda9f8cf6bc8c618d1e6b3295f64c31b4d67ea0
zhangzhewen <zhangzhewen@videopls.com>
2020-08-04 18:51:57 +0800

    QuickSort.java

--- src/main/java/org/zhangzhewen/algorithm/sort/QuickSort.java
+++ src/main/java/org/zhangzhewen/algorithm/sort/QuickSort.java
@@ -1,6 +1,142 @@
 package org.zhangzhewen.algorithm.sort;
 
-public class QuickSort extends Sortable{
+import static org.zhangzhewen.algorithm.sort.SortUtils.*;
+
+/**
+ * @author Varun Upadhyay (https://github.com/varunu28)
+ * @author Podshivalov Nikita (https://github.com/nikitap492)
+ * @see SortAlgorithm
+ */
+public class QuickSort implements SortAlgorithm {
+
+    /**
+     * This method implements the Generic Quick Sort
+     *
+     * @param array The array to be sorted
+     *              Sorts the array in increasing order
+     **/
+
+    @Override
+    public <T extends Comparable<T>> T[] sort(T[] array) {
+        doSort(array, 0, array.length - 1);
+        return array;
+    }
+
+
+    /**
+     * The sorting process
+     *
+     * @param left  The first index of an array
+     * @param right The last index of an array
+     * @param array The array to be sorted
+     **/
+
+    private static <T extends Comparable<T>> void doSort(T[] array, int left, int right) {
+        if (left < right) {
+            int pivot = randomPartition(array, left, right);
+            doSort(array, left, pivot - 1);
+            doSort(array, pivot, right);
+        }
+    }
+
+    /**
+     * Ramdomize the array to avoid the basically ordered sequences
+     *
+     * @param array The array to be sorted
+     * @param left  The first index of an array
+     * @param right The last index of an array
+     * @return the partition index of the array
+     */
+
+    private static <T extends Comparable<T>> int randomPartition(T[] array, int left, int right) {
+        int randomIndex = left + (int)(Math.random()*(right - left + 1));
+        swap(array, randomIndex, right);
+        return partition(array, left, right);
+    }
+
+    /**
+     * This method finds the partition index for an array
+     *
+     * @param array The array to be sorted
+     * @param left  The first index of an array
+     * @param right The last index of an array
+     *              Finds the partition index of an array
+     **/
+
+    private static <T extends Comparable<T>> int partition(T[] array, int left, int right) {
+        int mid = (left + right) >>> 1;
+        T pivot = array[mid];
+
+        while (left <= right) {
+            while (less(array[left], pivot)) {
+                ++left;
+            }
+            while (less(pivot, array[right])) {
+                --right;
+            }
+            if (left <= right) {
+                swap(array, left, right);
+                ++left;
+                --right;
+            }
+        }
+        return left;
+    }
+
+
+    public <T extends Comparable<T>> T[] sort2(T[] a) {
+
+
+        return a;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Driver Program
+    public static void main(String[] args) {
+
+        // For integer input
+        Integer[] array = {3, 4, 1, 32, 0, 1, 5, 12, 2, 5, 7, 8, 9, 2, 44, 111, 5};
+
+        QuickSort quickSort = new QuickSort();
+        quickSort.sort(array);
+
+        //Output => 0 1 1 2 2 3 4 5 5 5 7 8 9 12 32 44 111
+        print(array);
+
+        String[] stringArray = {"c", "a", "e", "b", "d"};
+        quickSort.sort(stringArray);
+
+        //Output => a	b	c	d	e
+        print(stringArray);
+    }
+}
+
+class QuickSort2 extends Sortable{
 
     public int[] sort2(int[] a) {
         return quickSort2(a,0,a.length-1);
@@ -28,24 +164,13 @@ public class QuickSort extends Sortable{
         return index-1;
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     public int[] sort(int[] a){
+
 
 
         return a;
     }
+
 
 
 
@@ -86,7 +211,7 @@ public class QuickSort extends Sortable{
 
 
     public static void main(String[] args) {
-        new QuickSort().print();
+        new QuickSort2().print();
     }
 
 
