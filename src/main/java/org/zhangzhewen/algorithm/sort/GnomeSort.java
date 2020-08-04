3f723c676d7e165b0d2eef1d979638990c370a51
zhangzhewen <zhangzhewen@videopls.com>
2020-08-04 14:46:32 +0800

    GnomeSort.java

---
+++ src/main/java/org/zhangzhewen/algorithm/sort/GnomeSort.java
@@ -0,0 +1,74 @@
+package org.zhangzhewen.algorithm.sort;
+
+import static org.zhangzhewen.algorithm.sort.SortUtils.*;
+
+/**
+ * Implementation of gnome sort
+ *
+ * @author Podshivalov Nikita (https://github.com/nikitap492)
+ * @since 2018-04-10
+ **/
+public class GnomeSort implements SortAlgorithm {
+
+    @Override
+    public <T extends Comparable<T>> T[] sort(T[] arr) {
+        int i = 1;
+        int j = 2;
+        while (i < arr.length) {
+            if (less(arr[i - 1], arr[i])) i = j++;
+            else {
+                swap(arr, i - 1, i);
+                if (--i == 0) {
+                    i = j++;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    <T extends Comparable<T>> void sort2(T[] a){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        Integer[] integers = {4, 23, 6, 78, 1, 26, 11, 23, 0, -6, 3, 54, 231, 9, 12};
+        String[] strings = {"c", "a", "e", "b", "d", "dd", "da", "zz", "AA", "aa", "aB", "Hb", "Z"};
+        GnomeSort gnomeSort = new GnomeSort();
+
+        gnomeSort.sort2(integers);
+        gnomeSort.sort2(strings);
+
+        System.out.println("After sort : ");
+        print(integers);
+        print(strings);
+
+
+    }
+
+}
