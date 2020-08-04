bf74a26ce269c6d27e2d47c173a8dc84edeacb44
zhangzhewen <zhangzhewen@videopls.com>
2020-08-04 16:27:56 +0800

    MergeSort.java

--- src/main/java/org/zhangzhewen/algorithm/sort/MergeSort.java
+++ src/main/java/org/zhangzhewen/algorithm/sort/MergeSort.java
@@ -1,73 +1,118 @@
 package org.zhangzhewen.algorithm.sort;
 
-import java.util.Arrays;
+import static org.zhangzhewen.algorithm.sort.SortUtils.print;
 
-public class MergeSort extends Sortable {
+/**
+ * This method implements the Generic Merge Sort
+ *
+ * @author Varun Upadhyay (https://github.com/varunu28)
+ * @author Podshivalov Nikita (https://github.com/nikitap492)
+ * @see SortAlgorithm
+ */
 
-    public int[] sort2(int[] a) {
+class MergeSort implements SortAlgorithm {
 
-        if (a.length < 2) {
-            return a;
-        }
+    /**
+     * This method implements the Generic Merge Sort
+     *
+     * @param unsorted the array which should be sorted
+     * @param <T>      Comparable class
+     * @return sorted array
+     */
+    @Override
 
-        int middle = a.length / 2;
-        int[] left = Arrays.copyOfRange(a, 0, middle);
-        int[] right = Arrays.copyOfRange(a, middle, a.length);
-
-        return merge2(sort(left), sort(right));
+    public <T extends Comparable<T>> T[] sort(T[] unsorted) {
+        doSort(unsorted, 0, unsorted.length - 1);
+        return unsorted;
     }
 
-    private int[] merge2(int[] left, int[] right) {
+    /**
+     * @param arr   The array to be sorted
+     * @param left  The first index of the array
+     * @param right The last index of the array
+     *              Recursively sorts the array in increasing order
+     **/
+    private static <T extends Comparable<T>> void doSort(T[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            doSort(arr, left, mid);
+            doSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
 
-        int[] result = new int[left.length + right.length];
-        int i = 0, l = 0, r = 0;
-        while (i < result.length) {
-            if (l == left.length) {
-                result[i++] = right[r++];
-            } else if (r == right.length) {
-                result[i++] = left[l++];
-            } else if (left[l] > right[r]) {
-                result[i++] = right[r++];
+    }
+
+    /**
+     * This method implements the merge step of the merge sort
+     *
+     * @param arr   The array to be sorted
+     * @param left  The first index of the array
+     * @param mid   The middle index of the array
+     * @param right The last index of the array
+     *              merges two parts of an array in increasing order
+     **/
+
+    private static <T extends Comparable<T>> void merge(T[] arr, int left, int mid, int right) {
+        int length = right - left + 1;
+        T[] temp = (T[]) new Comparable[length];
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i].compareTo(arr[j]) <= 0) {
+                temp[k++] = arr[i++];
             } else {
-                result[i++] = left[l++];
+                temp[k++] = arr[j++];
             }
         }
 
-        return result;
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        // Or
+//        int[] result = new int[left.length + right.length];
+//        int i = 0, l = 0, r = 0;
+//        while (i < result.length) {
+//            if (l == left.length) {
+//                result[i++] = right[r++];
+//            } else if (r == right.length) {
+//                result[i++] = left[l++];
+//            } else if (left[l] > right[r]) {
+//                result[i++] = right[r++];
+//            } else {
+//                result[i++] = left[l++];
+//            }
+//        }
+
+        System.arraycopy(temp, 0, arr, left, length);
     }
 
-    @Override
-    int[] sort(int[] a) {
+    <T extends Comparable<T>> void sort2(T[] a){
 
-
-        return null;
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // Driver program
     public static void main(String[] args) {
-        new MergeSort().print();
+
+        // Integer Input
+        Integer[] arr = {4, 23, 6, 78, 1, 54, 231, 9, 12};
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.sort2(arr);
+
+        // Output => 1	   4  	 6	9	12	23	54	78	231
+        print(arr);
+
+        // String Inpu
+        String[] stringArray = {"c", "a", "e", "b", "d"};
+        mergeSort.sort2(stringArray);
+        //Output => a	b	c	d	e
+        print(stringArray);
     }
 }
