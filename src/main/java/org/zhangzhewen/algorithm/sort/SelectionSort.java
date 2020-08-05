3e260486e0d783cb83b0427630af055531e2fae4
zhangzhewen <zhangzhewen@videopls.com>
2020-08-05 14:27:59 +0800

    SelectionSort.java

--- src/main/java/org/zhangzhewen/algorithm/sort/SelectionSort.java
+++ src/main/java/org/zhangzhewen/algorithm/sort/SelectionSort.java
@@ -1,43 +1,64 @@
 package org.zhangzhewen.algorithm.sort;
 
 /**
- * 描述
- *
- * @author zhangzhewen
- * @date 2019/1/14
+ * @author Varun Upadhyay (https://github.com/varunu28)
+ * @author Podshivalov Nikita (https://github.com/nikitap492)
+ * @see SortAlgorithm
  */
-public class SelectionSort extends Sortable{
 
+public class SelectionSort implements SortAlgorithm {
 
-    public int[] sort1(int[] a) {
+    /**
+     * This method swaps the two elements in the array
+     * @param <T>
+     * @param arr, i, j The array for the swap and
+    the indexes of the to-swap elements
+     */
 
-        int length = a.length;
-        for(int i = 0 ; i < length - 1 ; i ++){
+    public <T> void swap(T[] arr, int i, int j) {
+        T temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    /**
+     * This method implements the Generic Selection Sort
+     *
+     * @param arr The array to be sorted
+     *            Sorts the array in increasing order
+     **/
+    @Override
+    public <T extends Comparable<T>> T[] sort(T[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            // Initial index of min
             int min = i;
-            for(int j = i + 1; j <length ; j++){
-                if(a[j]<a[min]){
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[min].compareTo(arr[j]) < 0) {
                     min = j;
                 }
             }
-            if(i!=min){
-                int tmp = a[min];
-                a[min] = a[i];
-                a[i] = tmp;
+
+            // Swapping if index of min is changed
+            if (min != i) {
+                swap(arr, i, min);
             }
         }
-        return a;
+
+        return arr;
+    }
+
+
+    public <T extends Comparable<T>> T[] sort2(T[] arr) {
+
+        return arr;
     }
 
 
 
 
 
-    public int[] sort(int[] a){
-
-
-
-        return a;
-    }
 
 
 
@@ -46,9 +67,35 @@ public class SelectionSort extends Sortable{
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+    // Driver Program
     public static void main(String[] args) {
-        new SelectionSort().print();
+
+        Integer[] arr = {4, 23, 6, 78, 1, 54, 231, 9, 12};
+
+        SelectionSort selectionSort = new SelectionSort();
+
+        Integer[] sorted = selectionSort.sort(arr);
+
+        // Output => 1	  4	 6	9	12	23	54	78	231
+        SortUtils.print(sorted);
+
+        // String Input
+        String[] strings = {"c", "a", "e", "b", "d"};
+        String[] sortedStrings = selectionSort.sort(strings);
+
+        //Output => a	b	 c  d	e
+        SortUtils.print(sortedStrings);
     }
-
-
 }
