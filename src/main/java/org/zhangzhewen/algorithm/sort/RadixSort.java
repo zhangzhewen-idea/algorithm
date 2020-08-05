6d3aecbbd454c7cd9f6760e281d0b5d13dc1eb8b
zhangzhewen <zhangzhewen@videopls.com>
2020-08-05 11:48:23 +0800

    RadixSort.java

--- src/main/java/org/zhangzhewen/algorithm/sort/RadixSort.java
+++ src/main/java/org/zhangzhewen/algorithm/sort/RadixSort.java
@@ -2,7 +2,69 @@ package org.zhangzhewen.algorithm.sort;
 
 import java.util.*;
 
-public class RadixSort extends Sortable {
+import java.util.Arrays;
+
+public class RadixSort {
+
+    private static int getMax(int[] arr, int n) {
+        int mx = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > mx)
+                mx = arr[i];
+        return mx;
+    }
+
+    private static void countSort(int[] arr, int n, int exp) {
+        int[] output = new int[n];
+        int i;
+        int[] count = new int[10];
+        Arrays.fill(count, 0);
+
+        for (i = 0; i < n; i++)
+            count[(arr[i] / exp) % 10]++;
+
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+
+    private static void radixsort(int[] arr, int n) {
+
+        int m = getMax(arr, n);
+
+
+        for (int exp = 1; m / exp > 0; exp *= 10)
+            countSort(arr, n, exp);
+    }
+
+
+    static void print(int[] arr, int n) {
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+
+    static void radixsort2(int[] a,int len){
+
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {170, 45, 75, 90, 802, 24, 2, 66};
+        int n = arr.length;
+        radixsort2(arr, n);
+        print(arr, n);
+    }
+}
+// Written by James Mc Dermott(theycallmemac)
+
+class RadixSort2 extends Sortable {
 
     private static int getDigit (int[] array){
         //Getting Max Int at first.
@@ -64,7 +126,7 @@ public class RadixSort extends Sortable {
         }*/
 
 
-        new RadixSort().print();
+        new RadixSort2().print();
     }
 
 
