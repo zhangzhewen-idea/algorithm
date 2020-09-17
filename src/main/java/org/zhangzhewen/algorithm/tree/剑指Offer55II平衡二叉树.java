c4f9c08e745c3d207dfc6b5d980113be22e51c46
zhangzhewen <zhangzhewen@videopls.com>
2020-09-17 09:20:36 +0800

    剑指Offer55II平衡二叉树.java 相同的树100.java

---
+++ src/main/java/org/zhangzhewen/algorithm/tree/剑指Offer55II平衡二叉树.java
@@ -0,0 +1,73 @@
+package org.zhangzhewen.algorithm.tree;
+
+/**
+ * 输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定二叉树 [3,9,20,null,null,15,7]
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回 true 。
+ * <p>
+ * 示例 2:
+ * <p>
+ * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \
+ * 3   3
+ * / \
+ * 4   4
+ * 返回 false 。
+ *
+ * @author zhangzhewen
+ * @date 2020/9/14
+ */
+public class 剑指Offer55II平衡二叉树 {
+
+    public static void main(String[] args) {
+        new 剑指Offer55II平衡二叉树().isBalanced(new TreeNode(1,
+                        null,
+                        new TreeNode(2,
+                                null,
+                                new TreeNode(3,
+                                        null,
+                                        null
+                                )
+                        )
+                )
+        );
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return true;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
