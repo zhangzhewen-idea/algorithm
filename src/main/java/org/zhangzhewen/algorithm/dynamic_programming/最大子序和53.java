9d12ca208ae9bf483ca3bd3951858439a968f9b2
zhangzhewen <zhangzhewen@videopls.com>
2020-09-24 18:00:31 +0800

    最大子序和53.java

---
+++ src/main/java/org/zhangzhewen/algorithm/dynamic_programming/最大子序和53.java
@@ -0,0 +1,33 @@
+package org.zhangzhewen.algorithm.dynamic_programming;
+
+/**
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 示例:
+ *
+ * 输入: [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ * 进阶:
+ *
+ * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+ *
+ *
+ * @author zhangzhewen
+ * @date 2020/9/24
+ */
+public class 最大子序和53 {
+
+    public static void main(String[] args) {
+        new 最大子序和53().maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4});
+    }
+
+    public int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+}
