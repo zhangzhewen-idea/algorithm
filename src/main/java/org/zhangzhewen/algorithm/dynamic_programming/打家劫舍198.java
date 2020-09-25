aea3ee84ff25d619d73c7727f7d843c057684d3c
zhangzhewen <zhangzhewen@videopls.com>
2020-09-25 10:28:38 +0800

    打家劫舍198.java

--- src/main/java/org/zhangzhewen/algorithm/dynamic_programming/打家劫舍198.java
+++ src/main/java/org/zhangzhewen/algorithm/dynamic_programming/打家劫舍198.java
@@ -50,4 +50,25 @@ public class 打家劫舍198 {
         }
         return Math.max( dp[nums.length-1][0],dp[nums.length-1][1]);
     }
+
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int first = nums[0], second = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+
+
+
+
 }
