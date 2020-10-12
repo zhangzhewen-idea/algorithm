1b58abe02e055d6dd5889d1326f47568450629f9
zhangzhewen <zhangzhewen@videopls.com>
2020-10-12 10:49:31 +0800

    被围绕的区域130.java

--- src/main/java/org/zhangzhewen/algorithm/leetcode/被围绕的区域130.java
+++ src/main/java/org/zhangzhewen/algorithm/leetcode/被围绕的区域130.java
@@ -18,6 +18,41 @@ public class 被围绕的区域130 {
                         {'X','X','O','X','O'}
                 }
         );
+        new 被围绕的区域130().canMakeArithmeticProgression(new int[]{4,3,2,1});
+    }
+
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        for(int i = arr.length/2 ; i >=0 ;i--){
+            heapify(arr,i,arr.length-1);
+        }
+        for(int i = arr.length-1;i>0;i--){
+            int aux = arr[i];
+            arr[i] = arr[0];
+            arr[0] = aux;
+
+            heapify(arr,0,i-1);
+        }
+
+        System.out.println();
+        return true;
+
+    }
+    void heapify(int[] arr,int i,int len){
+        int l = i*2+1;
+        int r = i*2+2;
+        int max = i;
+        if(l<=len&&arr[l]>arr[max]){
+            max = l;
+        }
+        if(r<=len&&arr[r]>arr[max]){
+            max = r;
+        }
+        if(max!=i){
+            int aux = arr[max];
+            arr[max]=arr[i];
+            arr[i]=aux;
+            heapify(arr,max,len);
+        }
     }
 
     int n, m;
