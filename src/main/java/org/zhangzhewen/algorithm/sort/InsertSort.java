04dbc4eb91d8053212f9fede2c02e86ccf72465e
zhangzhewen <zhangzhewen@videopls.com>
2021-04-29 17:03:30 +0800

    InsertSort.java

--- src/main/java/org/zhangzhewen/algorithm/sort/InsertSort.java
+++ src/main/java/org/zhangzhewen/algorithm/sort/InsertSort.java
@@ -3,9 +3,13 @@ package org.zhangzhewen.algorithm.sort;
 public class InsertSort extends Sortable {
 
 
+    /*
+    更优化的有：1.折半插入 2.更优的 2路插入
+    http://c.biancheng.net/view/3441.html
+     */
     public int[] sort2(int[] a) {
         int length = a.length;
-        for (int i = 1,j,tmp; i < length; i++) {
+        for (int i = 1, j, tmp; i < length; i++) {
             tmp = a[i];
             j = i;
             while (j > 0 && tmp < a[j - 1]) {
@@ -20,25 +24,9 @@ public class InsertSort extends Sortable {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public int[] sort(int[] a) {
 
-
-return a;
-
+        return a;
     }
 
 
